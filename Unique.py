75d6b41ef02d61666d6c8906e2317758fec18b51
Riley <helnerr18@students.ecu.edu>
2022-10-30 20:14:18 -0400

    changes

--- Unique.py
+++ Unique.py
@@ -12,10 +12,7 @@ database='dvdrental'
 @app.route("/api/unique")
 
 def index():
-    # this is your index page
-    # connect to DB
     cursor, connection = util.connect_to_db(username,password,host,port,database)
-    # execute SQL commands
     record = util.run_and_fetch_sql(cursor, "SELECT a, fruit_a, b, fruit_b FROM basket_a FULL OUTER JOIN basket_b ON fruit_a = fruit_b WHERE a IS NULL OR b IS NULL;")
     if record == -1:
         
@@ -25,19 +22,12 @@ def index():
         connection.commit()
         col_names = [desc[0] for desc in cursor.description]
         
-        # only use the first five rows
         log = record[:5]
-        # log=[[1,2],[3,4]]
-    # disconnect from database
     util.disconnect_from_db(connection,cursor)
-    # using render_template function, Flask will search
-    # the file named index.html under templates folder
     return render_template('index.html', sql_table = log, table_title=col_names)
 
 
 if __name__ == '__main__':
-    # set debug mode
     app.debug = True
-    # your local machine ip
     ip = '127.0.0.1'
     app.run(host=ip)
